cf26dc4b4e1d98086fb93f5d2440705a6a98fee3
Alireza <alirezarashnoo85@gmail.com>
2026-09-13 19:54:15 +0330

    feat(users): handle duplicate user errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
@@ -26,15 +26,23 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 12);
 
-    const user = await this.prisma.user.create({
-      data: {
-        ...createUserDto,
-        password: hashedPassword,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          ...createUserDto,
+          password: hashedPassword,
+        },
+      });
 
-    const { password, ...result } = user;
+      const { password, ...result } = user;
 
-    return result;
+      return result;
+    } catch (error) {
+      if (error instanceof Error && 'code' in error && error.code === 'P2002') {
+        throw new ConflictException('Username or email already exists');
+      }
+
+      throw error;
+    }
   }
 }
