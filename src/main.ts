1c6e43d32a93a7a33479390e5ad82d807b3e9683
Alireza <alirezarashnoo85@gmail.com>
2026-09-26 14:36:22 +0330

    fix: correct validation pipe configuration

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,29 @@ import 'dotenv/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    transform: true,
   });
+
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
@@ -32,7 +37,6 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
